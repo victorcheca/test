962e958ba197085d33e779dd8670be7c4881af71
victorcheca <victorsisoncheca@gmail.com>
2022-04-25 08:12:24 +0200

    scrollnav.js

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -6,17 +6,21 @@ window.addEventListener('scroll', function(){
     // SUBIR
     document.getElementById("header").style.transform="translateY(0px)";
     document.getElementById("blog-nav").style.top="75px";
-    document.getElementById("header").style.transition=".9s";
-    document.getElementById("blog-nav").style.transition=".9s";
+    document.getElementById("header").style.transition=".5s";
+    document.getElementById("blog-nav").style.transition=".5s";
+    document.getElementById("header").style.transitionDelay="1s";
+    document.getElementById("blog-nav").style.transitionDelay="1s";
    
   }
     
   else{
     // BAJAR
     document.getElementById("header").style.transform="translateY(-75px)";
-    document.getElementById("header").style.transition=".9s";
     document.getElementById("blog-nav").style.top="0px";
-    document.getElementById("blog-nav").style.transition=".9s";
+    document.getElementById("header").style.transition=".5s";
+    document.getElementById("blog-nav").style.transition=".5s";
+    document.getElementById("header").style.transitionDelay="1s";
+    document.getElementById("blog-nav").style.transitionDelay="1s";
    
   }
   scrollPos = (document.body.getBoundingClientRect()).top;
